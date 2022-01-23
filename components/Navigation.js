41470081afc2c28b6e8db658f10c9c85a4268ea6
altela <altela_pramardhika@yahoo.com>
2022-01-24 00:50:08 +0800

    Fixed navigation gap

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,7 +4,7 @@ import React from "react"
 const Navigation = () => {
     return (
         <div className="sticky top-0 z-20 py-2 bg-black md:py-3 md:mb-6">
-            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center gap-x-10">
+            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center gap-x-8">
                 <Link href="/">
                     <a className={"font-large tracking-wider transition-colors text-white hover:text-sky-500 uppercase"} >
                     ◆
