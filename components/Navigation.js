e854559c1a8e598314b094d6b0c1a9c14f81d7a5
altela <altela_pramardhika@yahoo.com>
2022-01-24 01:22:59 +0800

    Fixed navigation gap

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,25 +4,39 @@ import React from "react"
 const Navigation = () => {
     return (
         <div className="sticky top-0 z-20 py-2 bg-black md:py-3 md:mb-6">
-            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center gap-x-8">
-                <Link href="/">
-                    <a className={"font-large tracking-wider transition-colors text-white hover:text-sky-500 uppercase"} >
-                    ◆
-                    </a>
-                </Link>
+            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center">
+                <div class="grid gap-x-5 grid-cols-3">
+                    <div>
+                        <Link href="/">
+                        <a className={"font-large tracking-wider transition-colors text-white hover:text-sky-500 uppercase"} >
+                        ◆
+                        </a>
+                    </Link>
+                    </div>
 
-                <Link href="/about">
-                <a className={"font-medium tracking-wider transition-colors text-white hover:text-sky-500 uppercase"} >
-                        About
-                    </a>
-                </Link>
+                    <div>
+                            <Link href="/about">
+                        <a className={"font-medium tracking-wider transition-colors text-white hover:text-sky-500 uppercase"} >
+                                About
+                            </a>
+                        </Link>
+                    </div>
+                    <div>
 
-                <Link href="/projects">
+                    <Link href="/projects">
                 <a className={"font-medium tracking-wider transition-colors text-white hover:text-sky-500 uppercase"} >
                         Projects
                     </a>
 
                 </Link>
+                    </div>
+
+                </div>
+
+
+
+
+
 
 
 
