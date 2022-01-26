5381a4515cab425ca08763353bbe99a03233a632
altela <altela_pramardhika@yahoo.com>
2022-01-27 02:14:54 +0800

    Added experience tab

--- pages/projects.js
+++ pages/projects.js
@@ -34,31 +34,95 @@ import Image from 'next/image'
 export default function Projects() {
   return (
     <div className="container mx-auto lg:max-w-4xl px-5">
-      <h2 class="text-3xl font-medium leading-tight mt-0 mb-2 text-gray-900 mt-20">Odoo ERP Modules</h2>
+      <h2 class="text-2xl font-medium leading-tight mt-0 text-gray-900 mt-20">Odoo ERP Modules</h2>
+        <div class="grid gap-2 grid-cols-3">
+          <div class="relative group">
+            <a href="https://apps.odoo.com/apps/modules/15.0/adjustment_rename/" target="_blank">
+              <img src="/rename_adj.jpg"></img>
+              <div class="opacity-0 group-hover:opacity-70 duration-300 absolute left-0 bottom-0 right-0 z-10 flex justify-center items-end bg-black text-gray-200 text-sm">Allows user to rename in-progress Inventory Adjustment </div>
+            </a>
+          </div>
 
-<table>
+
+          <div class="relative group">
+            <a href="https://apps.odoo.com/apps/modules/12.0/base_replace/" target="_blank">
+              <img src="/banner-base.jpg"></img>
+              <div class="opacity-0 group-hover:opacity-70 duration-300 absolute left-0 bottom-0 right-0 z-10 flex justify-center items-end bg-black text-gray-200 text-sm">The base module of Customer and Vendor replacement</div>
+            </a>
+          </div>
+
+          <div class="relative group">
+            <a href="https://apps.odoo.com/apps/modules/12.0/replace_cust_so/" target="_blank">
+              <img src="/replace_cust.jpg" class="w-50"></img>
+              <div class="opacity-0 group-hover:opacity-70 duration-300 absolute left-0 bottom-0 right-0 z-10 flex text-center items-end bg-black text-gray-200 text-sm">Allows user to change Customer in validated Sales Order</div>
+            </a>
+          </div>
+
+        </div>
+
+        <h2 class="text-2xl font-medium leading-tight mt-0 mb-5 text-gray-900 mt-10">Code Editor</h2>
+          <div class="grid gap-2 grid-cols-3">
+            <div class="relative group">
+              <a href ="https://github.com/altela/sublime-pine-autocomplete">
+                <img src="/sublime.png" class="w-20"></img>
+                <div class="opacity-0 group-hover:opacity-70 duration-300 absolute left-0 bottom-0 right-0 z-10 bg-black text-gray-200 text-sm">Pinescript Autocomplete</div>
+              </a>
+            </div>
+          </div>
+
+    </div>
+
+
+)
+}
+
+
+
+      
+
+{/* <table>
   <tbody>
     <tr>
       <td>
 
-      <div class="relative group">
-        <a href="https://apps.odoo.com/apps/modules/15.0/adjustment_rename/" target="_blank">
-          <img src="/rename_adj.jpg" class="max-w-full max-h-full"></img>
-          <div class="opacity-0 group-hover:opacity-70 duration-300 absolute left-0 bottom-0 right-0 z-10 flex justify-center items-end bg-black text-gray-200 text-sm">Allows user to rename in-progress Inventory Adjustment </div>
-        </a>
-      </div>
 
       </td>
-      <th><img src="/banner-base.jpg" alt="mammals" class="max-w-full max-h-full" title="mammals"></img></th>
+
+      <td>
+
+      </td>
+
+      <td>
+
+      </td>
 
     </tr>
   </tbody>
 </table>
 
+<table>
+<tr>
+<h2 class="text-2xl font-medium leading-tight mt-0 mb-2 text-gray-900 mt-10">Code Editor</h2>
+</tr>
+<tr>
+
+<td>
+<div>
+
+      </td>
 
 
-    </div>
+</tr>
+</table> */}
+
+
+
+// </div>
+
+
 
     
-  )
-}
+
+    
+//   )
+// }
